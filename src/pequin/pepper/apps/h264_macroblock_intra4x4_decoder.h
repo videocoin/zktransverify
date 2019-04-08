ddd661f1243aa6accbe2580b37a02a0260e1782e
taras <tshchybo@gmail.com>
2019-04-08 13:35:44 +0300

    Remove redundant fields

--- src/pequin/pepper/apps/h264_macroblock_intra4x4_decoder.h
+++ src/pequin/pepper/apps/h264_macroblock_intra4x4_decoder.h
@@ -4,30 +4,19 @@
 #include "h264_macroblock_defs.h"
 
 struct In {
-    int slice_num;
-
     int qscale;
     int chroma_qp[2];   // QPc
 
     int8_t intra4x4_pred_mode_cache[5 * 8];
 
-    unsigned int topleft_samples_available;
-    unsigned int top_samples_available;
     unsigned int topright_samples_available;
-    unsigned int left_samples_available;
 
     int linesize, uvlinesize;
 
     int mb_x, mb_y;
     int mb_xy;
-    int mb_type;
-
-    int top_type;
-
-    int mb_field_decoding_flag;
 
     int chroma_pred_mode;
-    int intra16x16_pred_mode;
 
     /**
      * non zero coeff count cache.
@@ -35,20 +24,11 @@ struct In {
      */
     uint8_t non_zero_count_cache[15 * 8];
 
-    uint8_t top_borders[2][(16 * 3) * 2];
-
     ///< as a dct coefficient is int32_t in high depth, we need to reserve twice the space.
     int16_t mb[16 * 48 * 2];
-    int16_t mb_luma_dc[16 * 2];
 
     // END OF SLICE CONTEXT
-    int mb_stride;
-    int mb_height, mb_width;
 
-    int pixel_shift;    ///< 0 for 8-bit H264, 1 for high-bit-depth H264
-    int chroma_y_shift;
-
-    uint16_t slice_table[1023 * 1024]; // TODO: W and H are dummy values
     /**
      * block_offset[ 0..23] for frame macroblocks
      * block_offset[24..47] for field macroblocks
