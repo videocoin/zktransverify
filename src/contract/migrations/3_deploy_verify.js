0eb504af7d698eae0fd4badf608d9ef524698ee4
rampenke <rampenke@gmail.com>
2019-04-20 02:17:35 +0000

    removed unwanted truffle deployment

--- src/contract/migrations/3_deploy_verify.js
+++
@@ -1,7 +0,0 @@
-var Pairing = artifacts.require("./ZkTransVerify.sol");
-var ZkTransVerify = artifacts.require("./ZkTransVerify.sol");
-module.exports = function(deployer) {
-  deployer.deploy(Pairing);
-  deployer.link(Pairing, ZkTransVerify);
-  deployer.deploy(ZkTransVerify);
-};
