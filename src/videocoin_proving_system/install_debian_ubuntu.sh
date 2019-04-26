339c5a09c2e31d5d9378e642314b957206201711
Taras <tshchybo@gmail.com>
2019-04-26 20:05:26 +0300

    updating installation scripts

--- src/videocoin_proving_system/install_debian_ubuntu.sh
+++ src/videocoin_proving_system/install_debian_ubuntu.sh
@@ -4,7 +4,8 @@ set -e
 
 PACKAGES="build-essential gcc g++ gcc-multilib g++-multilib\
           libboost-all-dev libgmp10 libgmp-dev rar unrar-free libbz2-dev\
-          zlib1g zlib1g-dev libssl-dev cmake"
+          zlib1g zlib1g-dev libssl-dev cmake libx11-dev\
+	  yasm libass-dev libfdk-aac-dev libmp3lame-dev libopus-dev libvorbis-dev libvpx-dev libx264-dev libx265-dev"
 
 sudo apt-get install $PACKAGES
 
@@ -20,6 +21,7 @@ echo "" >> $HOME/.bashrc
 echo "# Custom variables for Videocoin" >> $HOME/.bashrc
 echo "export VIDEOCOIN_ENV=${VIDEOCOIN_ENV}" >> $HOME/.bashrc
 echo "" >> $HOME/.bashrc
+source $HOME/.bashrc
 
 cd thirdparty
 ./install_deps.sh
--- src/videocoin_proving_system/thirdparty/install_deps.sh
+++ src/videocoin_proving_system/thirdparty/install_deps.sh
@@ -38,18 +38,13 @@ cp --preserve=links libleveldb.* $DEPS_DIR/lib
 cp -r include/leveldb $DEPS_DIR/include
 cd $UP
 
-#libsnark
-echo "installing libsnark"
-[ ! -d libsnark ] && git clone https://github.com/scipr-lab/libsnark.git
-cp libsnark_compilerflag.patch libsnark
-cd libsnark
-git checkout dc78fdae02b437bb6c838a82f9261c49bbd7723e
-git submodule init && git submodule update
-git apply libsnark_compilerflag.patch
-mkdir build && cd build
-echo $DEPS_DIR
-cmake -DCMAKE_INSTALL_PREFIX=$DEPS_DIR -DWITH_PROCPS=OFF -DMULTICORE=ON -DCURVE=ALT_BN128 ..
-DESTDIR=$DEPS_DIR make install
+#aom
+echo "installing aom"
+[ ! -d aom ] && git clone https://aomedia.googlesource.com/aom
+cd aom/build
+cmake ..
+make 
+sudo make install
 cd $UP
 
 #ffmpeg
@@ -77,5 +72,19 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-nonfree \
   --disable-optimizations
 PATH="$HOME/bin:$PATH" make
-make install
+sudo make install
+cd $UP
+
+#libsnark
+echo "installing libsnark"
+[ ! -d libsnark ] && git clone https://github.com/scipr-lab/libsnark.git
+cp libsnark_compilerflag.patch libsnark
+cd libsnark
+git checkout dc78fdae02b437bb6c838a82f9261c49bbd7723e
+git submodule init && git submodule update
+git apply libsnark_compilerflag.patch
+mkdir build && cd build
+echo $DEPS_DIR
+cmake -DCMAKE_INSTALL_PREFIX=$DEPS_DIR -DWITH_PROCPS=OFF -DMULTICORE=ON -DCURVE=ALT_BN128 ..
+DESTDIR=$DEPS_DIR make install
 cd $UP
